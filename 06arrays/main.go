bcb8bf8bcb877b77937b5a4c63eff803e9e18c86
mohitKumar <mohitbansal191120000@gmail.com>
2025-01-07 19:39:31 +0530

    array

---
+++ 06arrays/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	// arrays are not something which go developers most usely, they have slices which they do use a lot
+	// 1st scenerio
+	var array [4]int // declaring a array of size 4, all elements will be initialized to 0 as int a type, for string it will be empty string
+	array[0] = 1     // assigning the 1 to 0th element and all other elements will
+	fmt.Println("array printing => ", array)
+	fmt.Println("*** length of array => ", len(array))
+
+	// 2nd scenerio
+
+	var array2 = [4]string{"mohit", "rohit"}
+	fmt.Println("array2 printing => ", array2)
+	fmt.Println("*** length of array2 => ", len(array2))
+
+}
