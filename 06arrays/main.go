643f157495a3a86486a235b7d37139b52ae0ecca
mohitKumar <mohitbansal191120000@gmail.com>
2025-01-07 19:44:33 +0530

    printing types of array in go

--- 06arrays/main.go
+++ 06arrays/main.go
@@ -9,11 +9,14 @@ func main() {
 	array[0] = 1     // assigning the 1 to 0th element and all other elements will
 	fmt.Println("array printing => ", array)
 	fmt.Println("*** length of array => ", len(array))
+	fmt.Printf("*** Type of array => %T\n", array)
+	fmt.Println()
 
 	// 2nd scenerio
 
 	var array2 = [4]string{"mohit", "rohit"}
 	fmt.Println("array2 printing => ", array2)
 	fmt.Println("*** length of array2 => ", len(array2))
+	fmt.Printf("*** Type of array2 => %T\n", array2)
 
 }
