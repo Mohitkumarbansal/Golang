24326f3959d76423aaeab3447fe7c730fe7c3c23
mohitKumar <mohitbansal191120000@gmail.com>
2025-01-15 19:51:31 +0530

    initial code for building ai

---
+++ 16buildapiingo/go.mod
@@ -0,0 +1,3 @@
+module buildingapi
+
+go 1.23.4
---
+++ 16buildapiingo/main.go
@@ -0,0 +1,25 @@
+package main
+
+// Model for course - file
+type Course struct {
+	CourseId    string  `json:"courseid"`
+	CourseName  string  `json:"coursename"`
+	CoursePrice int     `json:"price"`
+	Author      *Author `json:"author"`
+}
+
+type Author struct {
+	Fullname string `json:"fullname"`
+	Website  string `json:"website`
+}
+
+// fake DB
+var courses []Course
+
+func (c *Course) IsEmpty() bool {
+	return (c.CourseId == "" && c.CourseName == "")
+}
+
+func main() {
+
+}
